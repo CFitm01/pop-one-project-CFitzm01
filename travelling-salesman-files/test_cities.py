0a91220006ebe29ac1877f6f53cdc0520903e324
CFitm01 <56835338+CFitm01@users.noreply.github.com>
2019-11-26 17:54:35 +0000

    Testing Update
    General testing okay; shift cities has passed, although admittedly the code within had to be modified.
    Need to read more on how to properly test instance of a class, although the math and the code itself does appear to be doing its job.

--- travelling-salesman-files/test_cities.py
+++ travelling-salesman-files/test_cities.py
@@ -1,21 +1,43 @@
 import pytest
-from cities import *
+import shift_cities
+import compute_total_distance
+import copy
+#from cities import *
 
 
+road_map = [("Alabama", "Birmingham", 1.23, -4.55),\
+            ("Hawaii", "Honolulu", 21.31, -157.83),\
+            ("Arkansas", "Little Rock", 34.74, -69.77)]
+
+#
 def test_compute_total_distance():
     road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
                 ("Delaware", "Dover", 39.161921, -75.526755),\
-                ("Minnesota", "Saint Paul", 44.95, -93.094)]
+                ("Minnesota", "Saint Paul", 44.95, -93.094)] 
+    distance = compute_total_distance.compute_total_distance(road_map1)
     
-    assert compute_total_distance(road_map1)==\
-           pytest.approx(9.386+18.496+10.646, 0.01)
+    assert distance == pytest.approx(9.386+18.496+10.646, 0.01)
+#
+#    '''add your further tests'''
+#
+#def test_swap_cities():
+#    #We want to ensure that the cities have indeed swapped
+#    #Further we want to ensure that the length is not changed,
+#    #nothing added or lost.
+#    assert len(swap_cities(road_map_1, 0, -1)) == len(road_map1)
+#    assert swap_cities(road_map)[2] != road_map[2]
+#    '''add your tests'''
+#
+#def test_shift_cities():
+#    road_map1 = copy.deepcopy(road_map)
+#    road_map2 = shift_cities.shift_cities(road_map1)
+#    #relatively simple test; we can still check whether or not the len remains
+#    #the same.   
+#    assert len(road_map2) == len(road_map)
+#    #Could test if there has been a change at all?
+#    assert road_map1[0] != road_map[0]
+#    '''add your tests'''
 
-    '''add your further tests'''
+#print(road_map[0][0])
 
-def test_swap_cities():
-    '''add your tests'''
-
-def test_shift_cities():
-    '''add your tests'''
-
-
+print(compute_total_distance.compute_total_distance(road_map))
