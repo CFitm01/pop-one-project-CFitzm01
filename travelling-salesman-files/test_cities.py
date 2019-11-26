0832d746171ad2f4db92f5899e4c493d490af2a2
CFitm01 <56835338+CFitm01@users.noreply.github.com>
2019-11-26 18:58:51 +0000

    All Module Tests
    Completed all module tests so far; at least to the point where each function has a couple of significant tests.
    Still needs to be managed in a way that befits a class instance, however.

--- travelling-salesman-files/test_cities.py
+++ travelling-salesman-files/test_cities.py
@@ -1,5 +1,6 @@
 import pytest
 import shift_cities
+import swap_cities
 import compute_total_distance
 import copy
 #from cities import *
@@ -17,27 +18,27 @@ def test_compute_total_distance():
     distance = compute_total_distance.compute_total_distance(road_map1)
     
     assert distance == pytest.approx(9.386+18.496+10.646, 0.01)
+    assert distance > 0
 #
 #    '''add your further tests'''
 #
-#def test_swap_cities():
-#    #We want to ensure that the cities have indeed swapped
-#    #Further we want to ensure that the length is not changed,
-#    #nothing added or lost.
-#    assert len(swap_cities(road_map_1, 0, -1)) == len(road_map1)
-#    assert swap_cities(road_map)[2] != road_map[2]
-#    '''add your tests'''
+def test_swap_cities():
+    #We want to ensure that the cities have indeed swapped
+    #Further we want to ensure that the length is not changed,
+    #nothing added or lost.
+    road_map1 = copy.deepcopy(road_map)
+    swap_cities.swap_cities(road_map1, 0, 2)
+    assert len(road_map1) == len(road_map)
+    assert road_map1[0] != road_map[0]
+    assert road_map1[1] == road_map[1]
+    '''add your tests'''
 #
-#def test_shift_cities():
-#    road_map1 = copy.deepcopy(road_map)
-#    road_map2 = shift_cities.shift_cities(road_map1)
-#    #relatively simple test; we can still check whether or not the len remains
-#    #the same.   
-#    assert len(road_map2) == len(road_map)
-#    #Could test if there has been a change at all?
-#    assert road_map1[0] != road_map[0]
-#    '''add your tests'''
-
-#print(road_map[0][0])
-
-print(compute_total_distance.compute_total_distance(road_map))
+def test_shift_cities():
+    road_map1 = copy.deepcopy(road_map)
+    road_map2 = shift_cities.shift_cities(road_map1)
+    #relatively simple test; we can still check whether or not the len remains
+    #the same.   
+    assert len(road_map2) == len(road_map)
+    #Could test if there has been a change at all?
+    assert road_map1[0] != road_map[0]
+    
